01981a4f2aa62cbc2e26091113e0f31118485350
steven2308 <steven2308@outlook.com>
2023-09-28 16:54:02 -0500

    Updates scripts.

--- scripts/deployProxy.ts
+++ scripts/deployProxy.ts
@@ -1,80 +1,49 @@
-import { bytecode } from "../artifacts/contracts/EmoteRepository.sol/EmoteRepository.json";
+import { bytecode } from "../artifacts/contracts/AttributesRepository.sol/AttributesRepository.json";
 import { ethers, run } from "hardhat";
 import { create2Address } from "../utils/utils";
 import { Wallet, Signer } from "ethers";
-import { delay } from "@nomiclabs/hardhat-etherscan/dist/src/etherscan/EtherscanService";
+import { delay } from '@nomiclabs/hardhat-etherscan/dist/src/etherscan/EtherscanService';
 
 async function main() {
-  console.log("Deploying DeployProxy...");
+    console.log("Deploying DeployProxy...");
 
-  // const deployer = await ethers.getSigner("0xbfa07ae67d7085b72352ff480bde8ce8705d9e27");
-  // console.log(deployer.address);
+    // const prefundedAddress = await ethers.getSigners();
+    // const deployer = new Wallet(process.env.REPOSITORY_DEPLOYER, prefundedAddress[0].provider);
+    const accounts: Signer[] = await ethers.getSigners();
+    const deployer = accounts[0];
+    console.log(`Deployer Address: ${await deployer.getAddress()}`);
 
-  // const prefundedAddress = await ethers.getSigners();
-  // const deployer = new Wallet(process.env.REPOSITORY_DEPLOYER, prefundedAddress[0].provider);
-  const accounts: Signer[] = await ethers.getSigners();
-  const deployer = accounts[0];
-  console.log(`Deployer Address: ${await deployer.getAddress()}`);
+    const DeployProxy = await ethers.getContractFactory("DeployProxy");
+    const deployProxy = await DeployProxy.connect(deployer).deploy(); // Used for the networks where the deploy proxy is not deployed to
+    // const deployProxy = DeployProxy.attach('0xe68fe566f9585c08F0123F92f111F6a3c00f6660'); // Used for the networks where the deploy proxy is already deployed to
 
-  // This is a utility to get the deployer to have some ETH to deploy the contract in the Hardhat's emualted network
-  // const [fundedAddress] = await ethers.getSigners();
-  // await fundedAddress.sendTransaction({ to: deployer.address, value: ethers.utils.parseEther("1.0") });
+    await deployProxy.deployed();
+    console.log(`DeployProxy deployed to: ${deployProxy.address}`);
 
-  const DeployProxy = await ethers.getContractFactory("DeployProxy");
-  // const deployProxy = await DeployProxy.connect(deployer).deploy(); // Used for the networks where the deploy proxy is not deployed to
-  const deployProxy = DeployProxy.attach(
-    "0xe68fe566f9585c08F0123F92f111F6a3c00f6660"
-  ); // Used for the networks where the deploy proxy is already deployed to
+    const initCode = bytecode;
 
-  await deployProxy.deployed();
-  console.log(`DeployProxy deployed to: ${deployProxy.address}`);
+    const saltHex = ethers.utils.id("7801454");
 
-  const initCode = bytecode;
+    const repositoryDeploy = await deployProxy.connect(deployer).deployContract(initCode, saltHex);
+    const transactionReceipt = await repositoryDeploy.wait();
 
-  // // This is used to find the saltHex that will generate the create2 address that starts with 0x3110735
-  // let saltHex = ethers.utils.id("0");
-  // // i used to generate the saltHex resulting in 0x31107354b61A0412E722455A771bC462901668eA is 15587301
-  // // The saltHex is generated using ethers.utils.id(i.toString())
-  // // The saltHex is 0x01d5f8344b54b86c283e72ccd7a83d4531817d6d761f6d0b2bc5d2770acf4aa9
-  // for(let i = 0; i < 1000000000; i++) {
-  //     // console.log(`Salt: ${saltHex}`);
-  //     const create2Addr = await create2Address(deployProxy.address, saltHex, initCode);
-  //     // console.log(`Precomputed create2 Address: ${create2Addr}`);
-  //     console.log(create2Addr.slice(2, 9));
-  //     if(create2Addr.slice(2, 9) === "3110735"){
-  //         console.log("Found!");
-  //         console.log(`Salt: ${saltHex}`);
-  //         console.log(`Precomputed create2 Address: ${create2Addr}`);
-  //         console.log(`i is: ${i}`);
-  //         break;
-  //     }
-  //     saltHex = ethers.utils.id((i + 1).toString());
-  // }
+    const repositoryAddress = transactionReceipt.events[0].args[0];
 
-  const saltHex = ethers.utils.id("96591818");
+    console.log(`Deployed EmoteRepository to: ${repositoryAddress}`);
 
-  const repositoryDeploy = await deployProxy
-    .connect(deployer)
-    .deployContract(initCode, saltHex);
-  const transactionReceipt = await repositoryDeploy.wait();
+    await delay(60000);
 
-  const repositoryAddress = transactionReceipt.events[0].args[0];
-
-  console.log(`Deployed EmoteRepository to: ${repositoryAddress}`);
-
-  await delay(60000);
-
-  try {
-    await run("verify:verify", {
-      address: repositoryAddress,
-      constructorArguments: [],
-    });
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        await run('verify:verify', {
+          address: repositoryAddress,
+          constructorArguments: [],
+        });
+      } catch (error) {
+        console.log(error);
+      }
 }
 
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
